49742cf88d8f03991001023e7470eb928fb4882c
bedrockdev <dev.bedrock@gmail.com>
2019-07-22 22:52:10 +0900

    Added auto_upload.py

---
+++ auto_upload.py
@@ -0,0 +1,23 @@
+import moviepy.editor as mp
+import imageio
+imageio.plugins.ffmpeg.download()
+import glob
+import os
+from pathlib import Path
+
+home = str(Path.home())
+video_path = home + "/Videos/Logitech Webcam"
+audio_path = home + "/OneDrive/오디오파일"
+
+list_of_files = glob.glob(video_path + "/*.mp4")
+target = max(list_of_files, key=os.path.getctime)
+
+print(target)
+
+list_of_files = glob.glob(audio_path + "/*.mp3")
+target = max(list_of_files, key=os.path.getctime)
+
+print(target)
+
+#clip = mp.VideoFileClip(target)
+#clip.audio.write_audiofile("theaudio.mp3")
